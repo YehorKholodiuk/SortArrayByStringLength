78453c18816b2248689ff313124ad0f5705ff9bf
ASUS <Yehor Kholodiuk>
2022-08-05 16:25:10 +0300

    initial commit

--- SortArrayByStringLength.js
+++ SortArrayByStringLength.js
@@ -6,3 +6,7 @@ function sortByLength (array) {
 
 }
 console.log(sortByLength(["Beg", "Life", "I", "To"]))
+
+function sortByLength (array) {
+    return array.sort((a,b) => a.length - b.length);
+};
